d853b32e4f40141e3a50cb101c5811d09e2b7146
Hideki Yoshikura <evenintherain@gmail.com>
2021-05-08 12:53:48 +0900

    修正

---
+++ src/summit-restapi/WebApi/Migrations/20210508034244_Init1.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CfjSummit.WebApi.Migrations
+{
+    public partial class Init1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "RequestLogs",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Uid = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Method = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Controller = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ContentType = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    RequestBody = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    LocalIpAddress = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    RemoteIpAddress = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ModifiedAt = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    TimeStamp = table.Column<byte[]>(type: "rowversion", rowVersion: true, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RequestLogs", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Tracks",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    TrackGuid = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Name_Ja = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Name_En = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Name_Zh_Tw = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Name_Zh_Cn = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ModifiedAt = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    TimeStamp = table.Column<byte[]>(type: "rowversion", rowVersion: true, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tracks", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserProfiles",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Uid = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Name_Ja = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Name_Ja_Kana = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Name_En = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Name_Zh_Tw = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Name_Zh_Cn = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Role = table.Column<int>(type: "int", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ModifiedAt = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    TimeStamp = table.Column<byte[]>(type: "rowversion", rowVersion: true, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserProfiles", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Programs",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ProgramGuid = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Title_Ja = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Title_En = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Title_Zh_Tw = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Title_Zh_Cn = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ProgramCategory = table.Column<int>(type: "int", nullable: false),
+                    Description_Ja = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Description_En = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Description_Zh_Tw = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Description_Zh_Cn = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Date = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    StartTime = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    EndTime = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    TrackId = table.Column<long>(type: "bigint", nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ModifiedAt = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    TimeStamp = table.Column<byte[]>(type: "rowversion", rowVersion: true, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Programs", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Programs_Tracks_TrackId",
+                        column: x => x.TrackId,
+                        principalTable: "Tracks",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ProgramOwners",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ProgramId = table.Column<long>(type: "bigint", nullable: true),
+                    UserProfileId = table.Column<long>(type: "bigint", nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ModifiedAt = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    TimeStamp = table.Column<byte[]>(type: "rowversion", rowVersion: true, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProgramOwners", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ProgramOwners_Programs_ProgramId",
+                        column: x => x.ProgramId,
+                        principalTable: "Programs",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_ProgramOwners_UserProfiles_UserProfileId",
+                        column: x => x.UserProfileId,
+                        principalTable: "UserProfiles",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProgramOwners_ProgramId",
+                table: "ProgramOwners",
+                column: "ProgramId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProgramOwners_UserProfileId",
+                table: "ProgramOwners",
+                column: "UserProfileId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Programs_TrackId",
+                table: "Programs",
+                column: "TrackId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ProgramOwners");
+
+            migrationBuilder.DropTable(
+                name: "RequestLogs");
+
+            migrationBuilder.DropTable(
+                name: "Programs");
+
+            migrationBuilder.DropTable(
+                name: "UserProfiles");
+
+            migrationBuilder.DropTable(
+                name: "Tracks");
+        }
+    }
+}
