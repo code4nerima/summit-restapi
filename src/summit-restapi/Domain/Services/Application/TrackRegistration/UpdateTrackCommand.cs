d405353d3ffff4083e642325ced088cd8df4f838
Hideki Yoshikura <evenintherain@gmail.com>
2021-05-10 01:12:10 +0900

    updatetrack追加

---
+++ src/summit-restapi/Domain/Services/Application/TrackRegistration/UpdateTrackCommand.cs
@@ -0,0 +1,14 @@
+﻿using CfjSummit.Domain.Models.DTOs.Tracks;
+using MediatR;
+
+namespace CfjSummit.Domain.Services.Application.TrackRegistration
+{
+    public class UpdateTrackCommand : IRequest<string>
+    {
+        public TrackDTO TrackDTO { get; set; }
+        public UpdateTrackCommand(TrackDTO dto)
+        {
+            TrackDTO = dto;
+        }
+    }
+}
---
+++ src/summit-restapi/Domain/Services/Application/TrackRegistration/UpdateTrackCommandHandler.cs
@@ -0,0 +1,30 @@
+﻿using CfjSummit.Domain.Repositories;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CfjSummit.Domain.Services.Application.TrackRegistration
+{
+    public class UpdateTrackCommandHandler : IRequestHandler<UpdateTrackCommand, string>
+    {
+        private readonly ITrackRepository _repository;
+
+        public UpdateTrackCommandHandler(ITrackRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<string> Handle(UpdateTrackCommand request, CancellationToken cancellationToken)
+        {
+            var track = await _repository.GetAll().SingleOrDefaultAsync(x => x.TrackGuid == request.TrackDTO.TrackGuid);
+            if (track == null) { throw new Exception(); }
+            track.Update(request.TrackDTO);
+            _repository.Update(track);
+            await _repository.SaveChangesAsync();
+            return track.TrackGuid;
+
+        }
+    }
+}
