8713fb03b2ada3a1b4f8173d059a8447151be5cc
Hideki Yoshikura <evenintherain@gmail.com>
2021-05-17 21:51:15 +0900

    fix

--- src/summit-restapi/Domain/Services/Application/ProgramRegistration/UpdateProgramCommandHandler.cs
+++ src/summit-restapi/Domain/Services/Application/ProgramRegistration/UpdateProgramCommandHandler.cs
@@ -1,4 +1,5 @@
-﻿using CfjSummit.Domain.Repositories;
+﻿using CfjSummit.Domain.Models.Entities;
+using CfjSummit.Domain.Repositories;
 using MediatR;
 using System;
 using System.Linq;
@@ -10,10 +11,12 @@ namespace CfjSummit.Domain.Services.Application.ProgramRegistration
     public class UpdateProgramCommandHandler : IRequestHandler<UpdateProgramCommand, string>
     {
         private readonly IProgramRepository _repository;
+        private readonly IQueryableRepository<Track> _trackRepository;
 
-        public UpdateProgramCommandHandler(IProgramRepository repository)
+        public UpdateProgramCommandHandler(IProgramRepository repository, IQueryableRepository<Track> trackRepository)
         {
             _repository = repository;
+            _trackRepository = trackRepository;
         }
 
         public async Task<string> Handle(UpdateProgramCommand request, CancellationToken cancellationToken)
@@ -21,6 +24,13 @@ namespace CfjSummit.Domain.Services.Application.ProgramRegistration
             var p = _repository.GetAll().SingleOrDefault(x => x.ProgramGuid == request.ProgramPartsDataDTO.ProgramGuid);
             if (p == null) { throw new Exception(); }
             p.Update(request.ProgramPartsDataDTO);
+
+            var track = _trackRepository.GetAll().SingleOrDefault(x => x.TrackGuid == request.ProgramPartsDataDTO.TrackGuid);
+            if (track != null)
+            {
+                p.SetTrackId(track.Id);
+            }
+
             _repository.Update(p);
             _ = await _repository.SaveChangesAsync();
             return p.ProgramGuid;
