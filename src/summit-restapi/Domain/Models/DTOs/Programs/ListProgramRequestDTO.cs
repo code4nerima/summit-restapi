5da89db47d4a39da37633a6dc25fd202dae774d0
Hideki Yoshikura <evenintherain@gmail.com>
2021-06-12 09:07:21 +0900

    修正

--- src/summit-restapi/Domain/Models/DTOs/Programs/ListProgramRequestDTO.cs
+++ src/summit-restapi/Domain/Models/DTOs/Programs/ListProgramRequestDTO.cs
@@ -1,6 +1,13 @@
-﻿namespace CfjSummit.Domain.Models.DTOs.Programs
+﻿using System.Text.Json.Serialization;
+
+namespace CfjSummit.Domain.Models.DTOs.Programs
 {
     public class ListProgramRequestDTO : ListRequestDTO
     {
+        [JsonPropertyName("programOwnerUid")]
+        public string ProgramOwnerUid { get; set; }
+
+        [JsonPropertyName("programMemberUid")]
+        public string ProgramMemberUid { get; set; }
     }
 }
