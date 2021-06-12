c40a91b96f9aae956671f0623bf585f6d8d9a60e
Hideki_Yoshikura <34097009+jinka1997@users.noreply.github.com>
2021-06-13 07:01:49 +0900

    Trackに項目追加 (#14)

---
+++ src/summit-restapi/WebApi/Migrations/20210612092013_Change-2021-06-12-01.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CfjSummit.WebApi.Migrations
+{
+    public partial class Change2021061201 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "BroadcastingURL",
+                table: "Tracks",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "MeetingId",
+                table: "Tracks",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "MeetingPasscode",
+                table: "Tracks",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "MeetingUrl",
+                table: "Tracks",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Station",
+                table: "Tracks",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "StreamKey",
+                table: "Tracks",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "StreamUrl",
+                table: "Tracks",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BroadcastingURL",
+                table: "Tracks");
+
+            migrationBuilder.DropColumn(
+                name: "MeetingId",
+                table: "Tracks");
+
+            migrationBuilder.DropColumn(
+                name: "MeetingPasscode",
+                table: "Tracks");
+
+            migrationBuilder.DropColumn(
+                name: "MeetingUrl",
+                table: "Tracks");
+
+            migrationBuilder.DropColumn(
+                name: "Station",
+                table: "Tracks");
+
+            migrationBuilder.DropColumn(
+                name: "StreamKey",
+                table: "Tracks");
+
+            migrationBuilder.DropColumn(
+                name: "StreamUrl",
+                table: "Tracks");
+        }
+    }
+}
