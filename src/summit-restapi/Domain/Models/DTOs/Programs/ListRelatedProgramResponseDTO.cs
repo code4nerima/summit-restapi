1f52653b94bd8157c8eabf3f7ef7e7bc67c41964
Hideki_Yoshikura <34097009+jinka1997@users.noreply.github.com>
2021-07-04 18:32:25 +0900

    add (#26)

---
+++ src/summit-restapi/Domain/Models/DTOs/Programs/ListRelatedProgramResponseDTO.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace CfjSummit.Domain.Models.DTOs.Programs
+{
+    public class ListRelatedProgramResponseDTO
+    {
+        [JsonPropertyName("ownerOfPrograms")]
+        public List<string> OwnerOfPrograms { get; set; }
+
+        [JsonPropertyName("memberOfPrograms")]
+        public List<string> MemberOfPrograms { get; set; }
+
+        [JsonPropertyName("submittedProgram")]
+        public List<string> SubmittedProgram { get; set; }
+    }
+}
