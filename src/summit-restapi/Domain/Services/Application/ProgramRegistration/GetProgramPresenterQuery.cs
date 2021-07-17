e9244bb422c8ab7ce3b0c401901e342d2de10df0
Hideki Yoshikura <evenintherain@gmail.com>
2021-07-17 10:04:14 +0900

    Get追加

---
+++ src/summit-restapi/Domain/Services/Application/ProgramRegistration/GetProgramPresenterQuery.cs
@@ -0,0 +1,40 @@
+﻿using CfjSummit.Domain.Models.DTOs.Programs.Attatchments;
+using CfjSummit.Domain.Models.Entities;
+using CfjSummit.Domain.Repositories;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CfjSummit.Domain.Services.Application.ProgramRegistration
+{
+    public class GetProgramPresenterQuery : IRequest<ProgramPresenterDTO>
+    {
+        public GetProgramPresenterQuery(ProgramPresenterKeyDataDTO dto)
+        {
+            ProgramPresenterKeyDataDTO = dto;
+        }
+
+        public ProgramPresenterKeyDataDTO ProgramPresenterKeyDataDTO { get; }
+    }
+    internal class GetProgramPresenterQueryHandler : IRequestHandler<GetProgramPresenterQuery, ProgramPresenterDTO>
+    {
+        private readonly IQueryableRepository<Program> _repository;
+        public GetProgramPresenterQueryHandler(IQueryableRepository<Program> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<ProgramPresenterDTO> Handle(GetProgramPresenterQuery request, CancellationToken cancellationToken)
+        {
+            return await _repository.GetAll()
+                                    .Include(x => x.ProgramPresenters)
+                                    .Where(x => x.ProgramGuid == request.ProgramPresenterKeyDataDTO.ProgramGuid)
+                                    .SelectMany(x => x.ProgramPresenters)
+                                    .Where(x => x.ProgramPresenterGuid == request.ProgramPresenterKeyDataDTO.ProgramPresenterGuid)
+                                    .Select(x => ProgramPresenterDTO.CreateDto(x))
+                                    .SingleOrDefaultAsync(cancellationToken: cancellationToken);
+        }
+    }
+}
---
+++ src/summit-restapi/WebApi/Controllers/GetPresenterController.cs
@@ -0,0 +1,49 @@
+﻿using CfjSummit.Domain.Models.DTOs.Programs.Attatchments;
+using CfjSummit.Domain.Services.Application.ProgramRegistration;
+using CfjSummit.WebApi.Interfaces;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace CfjSummit.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GetPresenterController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public GetPresenterController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost]
+        public async ValueTask<ActionResult<GetPresenterResponse>> PostAsync([FromBody] GetPresenterRequest request, [FromHeader] string authorization)
+        {
+            await _mediator.Send(Logger.CreateWriteLogCommand(Request, request));
+            if (!Authorization.Authorized(authorization)) { return Unauthorized(); }
+            var query = new GetProgramPresenterQuery(request.Data);
+            var item = await _mediator.Send(query);
+
+            return new GetPresenterResponse()
+            {
+                Data = item
+            };
+        }
+
+
+    }
+    public class GetPresenterRequest : AbstractRequestBody
+    {
+        [JsonPropertyName("data")]
+        public ProgramPresenterKeyDataDTO Data { get; set; }
+    }
+    public class GetPresenterResponse : AbstractResponseBody
+    {
+        [JsonPropertyName("data")]
+        public ProgramPresenterDTO Data { get; set; }
+    }
+
+}
