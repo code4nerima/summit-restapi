5c6e2f0aa44ed6828a2cf51dc2ce50c0a2622a45
Hideki Yoshikura <evenintherain@gmail.com>
2021-08-09 12:11:23 +0900

    追加

---
+++ src/summit-restapi/WebApi/Controllers/ListProgramForWebController.cs
@@ -0,0 +1,45 @@
+﻿using CfjSummit.Domain.Models.DTOs.Programs;
+using CfjSummit.Domain.Services.Application.ProgramRegistration;
+using CfjSummit.WebApi.Interfaces;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace CfjSummit.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ListProgramForWebController : ControllerBase
+    {
+
+        private readonly IMediator _mediator;
+
+        public ListProgramForWebController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+        [HttpPost]
+        public async ValueTask<ActionResult<ListProgramForWebResponse>> PostAsync([FromHeader] string authorization,
+            [FromQuery] string lang, [FromQuery] string date)
+        {
+            await _mediator.Send(Logger.CreateWriteLogCommand(Request, ""));
+            if (!Authorization.Authorized(authorization)) { return Unauthorized(); }
+            var query = new ListProgramForWebQuery(lang, date);
+            var item = await _mediator.Send(query);
+            return new ListProgramForWebResponse()
+            {
+                Data = item
+            };
+        }
+
+    }
+
+    public class ListProgramForWebResponse : AbstractResponseBody
+    {
+        [JsonPropertyName("data")]
+        public List<ListProgramForWebResponseDTO> Data { get; set; }
+    }
+
+}
